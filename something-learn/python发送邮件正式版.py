17960f65a53688d4f4620ec422ef8834b36e44b4
star <1694222672@qq.com>
2018-09-24 11:49:38 +0800

    v13 smtp,自己搭建的服务器

--- something-learn/python发送邮件正式版.py
+++ something-learn/python发送邮件正式版.py
@@ -2,14 +2,15 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-sender = 'admin@test.com'
+sender = 'qq@vipjiexi.club'
 receiver = ['1694222672@qq.com']
 
 msg = MIMEText('python邮件测试，本地到QQ邮箱....','plain','utf-8')
-msg['From'] = Header('ben','utf-8')
+msg['From'] = Header('qq','utf-8')
 msg['To'] = Header('qq','utf-8')
 msg['Subject'] = Header('python SMTP邮件测试','utf-8')
 
 
-smtpobj = smtplib.SMTP('localhost')
+smtpobj = smtplib.SMTP('smtp.vipjiexi.club')
+smtpobj.login('qq@vipjiexi.club','qjp12345')
 smtpobj.sendmail(sender,receiver,msg.as_string())
